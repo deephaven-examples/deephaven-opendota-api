b995508ed64e3c660990c725209a591fb8deb6d0
JJ Brosnan <84038776+jjbrosnan@users.noreply.github.com>
2022-06-17 16:33:08 -0400

    Very minor update to the Dota2Match class

--- data/app.d/Get_Match_Details.py
+++ data/app.d/Get_Match_Details.py
@@ -298,7 +298,7 @@ class D2M:
     def __init__(self, id):
         self.id = id
         self.details = None
-        self.players = None
+        self.Players = None
         self.teamfights = None
         self.chat = None
         self.draft = None
